b0261903d4913c152acd804792b746457680960f
Alex.Leung <alex.leung@nanfung.com>
2018-02-22 10:52:22 +0800

    Revert "Warp callback"
    This reverts commit 16e0644

--- src/utils/plist_parse.js
+++ src/utils/plist_parse.js
@@ -1,25 +1,17 @@
-const common = require('./common')
+const common = require('./common');
 
 /*
  * Parse .plist files into DOM
  */
-function parse (err, plist) {
-  return new Promise((resolve, rejected) => {
-    if (err) {rejected(err)}
-    resolve(plist)
-  })
-}
-
-module.exports = (plist, fileObj) => new Promise(async (resolve, reject) => {
-  try {
-    common.throwIfNotObject(plist, 'Provided plist parser is not object')
-    common.throwIfNotFuction(plist.parse, 'Provided plist do not have parse (parse()) function')
-    const content = await plist(fileObj.content, await parse)
-    resolve({
-      path: fileObj.path,
-      content: content
-    })
-  } catch (parseOrReadError) {
-    reject(parseOrReadError)
-  }
-})
+module.exports = (plist, fileObj) => new Promise((resolve, reject) => {
+    try {
+        common.throwIfNotObject(plist, 'Provided plist parser is not object');
+        common.throwIfNotFuction(plist.parse, 'Provided plist do not have parse (parse()) function');
+        resolve({
+            path: fileObj.path,
+            content: plist.parse(fileObj.content)
+        });
+    } catch (parseOrReadError) {
+        reject(parseOrReadError);
+    }
+});
